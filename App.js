2c290cd9b604ead8b23989f59732db13775d1c4c
G-Naik <121098220+G-Naik@users.noreply.github.com>
2023-03-26 13:52:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TaskTracker from './TaskTracker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+        <TaskTracker/>
+    </div>
+  );
+}
+
+export default App;
